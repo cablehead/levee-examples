bc319c76fb4a7e75718829dd2a9d42be6d8cfea3
Andy Gayton <andy@thecablelounge.com>
2016-05-22 12:56:53 -0700

    add initial chat example

---
+++ chat/1.lua
@@ -0,0 +1,19 @@
+local levee = require("levee")
+local p = levee.p
+
+local h = levee.Hub()
+
+local err, serve = h.stream:listen(9000)
+
+
+for conn in serve do
+		h:spawn(function()
+				local stream = conn:stream()
+				while true do
+					local err, line = p.line.stream(stream, "\r\n")
+					if err then break end
+					print(line)
+				end
+				conn:close()
+		end)
+end
